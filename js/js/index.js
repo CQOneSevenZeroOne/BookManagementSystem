a8408014df5ac2558564d2c7d1966f1af8d0a5bd
liumiemie <1316084816@qq.com>
2018-01-04 12:41:33 +0800

    提交

--- js/js/index.js
+++ js/js/index.js
@@ -1,9 +1,35 @@
 $(function(){
+	//头部
 	$("#header").load("html/header.html");
-<<<<<<< HEAD
-	$("#search_byAuthor").load("html/search_author.html");
+	//查询
+	/*$("#search_byAuthor").load("html/search_author.html");
 	$("#stu_search").load("html/stu_search.html");
-=======
-	$("#searchById").load("html/search_id.html");
->>>>>>> a6fb55e9cc2b5fa6c757b177c16e50fb6607ed75
+	$("#searchById").load("html/search_id.html");*/
+	//修改
+	$("#edit").click(function(){
+		$("#cont").load("html/student.html");
+	})
+	
+	$("#student_change").load("html/student_change.html");
+	$("#book_num").load("html/book_num.html");
+	$("#book_num_change").load("html/book_num_change.html");
+	$("#lend_state").load("html/lend_state.html");
+	$("#lend_state_change").load("html/lend_state_change.html");
+	$("#pass_change").load("html/pass_change.html");*/
+	//增加
+	$("#addbook").click(function(){
+		$("#cont").load("html/AddBook.html");
+	})
+	$("#addstu").click(function(){
+		$("#cont").load("html/AddStu.html");
+	})
+	
+	//删除
+	$("#deletebook").click(function(){
+		$("#cont").load("html/DeleteBook.html");
+	})
+	$("#deletestu").click(function(){
+		$("#cont").load("html/DeleteUser.html");
+	})
+	
 })
