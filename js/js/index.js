302dfe5e5a4dba8dfdda164c5aa76660f84ce2da
PiYujie <pyjpi@foxmail.com>
2018-01-04 12:45:16 +0800

    更新

--- js/js/index.js
+++ js/js/index.js
@@ -1,9 +1,6 @@
 $(function(){
 	$("#header").load("html/header.html");
-<<<<<<< HEAD
 	$("#search_byAuthor").load("html/search_author.html");
 	$("#stu_search").load("html/stu_search.html");
-=======
 	$("#searchById").load("html/search_id.html");
->>>>>>> a6fb55e9cc2b5fa6c757b177c16e50fb6607ed75
 })
