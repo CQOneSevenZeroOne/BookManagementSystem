3a91cdad61633834e01018e352afb7b3365e6f3e
liumiemie <1316084816@qq.com>
2018-01-04 14:49:44 +0800

    test

--- js/js/index.js
+++ js/js/index.js
@@ -2,17 +2,23 @@ $(function(){
 	//头部
 	$("#header").load("html/header.html");
 	
-	//修改
+	//修改学生信息
 	$("#edit").click(function(){
 		$("#cont").load("html/student.html");
 	})
+	//修改密码
+	$("#pass").click(function(){
+		$("#cont").load("html/pass_change.html");
+	})
+//	var student_operate=$("#student_info").lastChild();
+//	console.log(student_operate);
 	
-	$("#student_change").load("html/student_change.html");
-	$("#book_num").load("html/book_num.html");
-	$("#book_num_change").load("html/book_num_change.html");
-	$("#lend_state").load("html/lend_state.html");
-	$("#lend_state_change").load("html/lend_state_change.html");
-	$("#pass_change").load("html/pass_change.html");
+//	$("#student_change").load("html/student_change.html");
+//	$("#book_num").load("html/book_num.html");
+//	$("#book_num_change").load("html/book_num_change.html");
+//	$("#lend_state").load("html/lend_state.html");
+//	$("#lend_state_change").load("html/lend_state_change.html");
+	
 	//增加
 	$("#addbook").click(function(){
 		$("#cont").load("html/AddBook.html");
@@ -28,13 +34,4 @@ $(function(){
 	$("#deletestu").click(function(){
 		$("#cont").load("html/DeleteUser.html");
 	})
-	//通过作者查询图书信息
-	$("#search_byAuthor").load("html/search_author.html");
-	//学生信息查询
-	$("#stu_search").load("html/stu_search.html");
-	//借阅记录查询
-	$("#loaningRecord").load("html/loaningRecord.html")
-	//罚款管理
-	$("#penalty").load("html/penalty.html");
-	$("#searchById").load("html/search_id.html");
 })
